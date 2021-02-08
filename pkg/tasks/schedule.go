0e6478e71c52654d60d47400862951f5741dd047
Duncan Ashe <d.ashe@cointel.ai>
2021-02-08 16:10:25 +0100

    homebrew task scheduling

--- pkg/tasks/schedule.go
+++ pkg/tasks/schedule.go
@@ -1,45 +1,36 @@
 package tasks
 
 import (
-	"github.com/RichardKnop/machinery/v1/config"
-	"github.com/RichardKnop/machinery/v2"
+	"encoding/json"
 
-	redisbackend "github.com/RichardKnop/machinery/v1/backends/redis"
-	redisbroker "github.com/RichardKnop/machinery/v1/brokers/redis"
-	eagerlock "github.com/RichardKnop/machinery/v1/locks/eager"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
 
-func StartServer() (*machinery.Server, error) {
-	cnf := &config.Config{
-		DefaultQueue:    "machinery_tasks",
-		ResultsExpireIn: 3600,
-		Redis: &config.RedisConfig{
-			MaxIdle:                3,
-			IdleTimeout:            240,
-			ReadTimeout:            15,
-			WriteTimeout:           15,
-			ConnectTimeout:         15,
-			NormalTasksPollPeriod:  1000,
-			DelayedTasksPollPeriod: 500,
-		},
+type TaskMsg struct {
+	Type    string            `json:"type"`
+	Msg     json.RawMessage   `json:"heights"`
+}
+
+func (task TaskMsg) HandleTask {
+	switch {
+		case task.Type == "gather_blocks":
+			var cmd GatherBlocksMsg
+			errResp := json.Unmarshal(message, &cmd)
+			//Call Gather blocks tasks
+		case task.Type == "chain_tip":
+			var cmd ChainTipMsg
+			errResp := json.Unmarshal(message, &cmd)
+			//Call EmitBlocks to be gathered
 	}
+}
 
-	// Create server instance
-	broker := redisbroker.NewGR(cnf, []string{"localhost:6379"}, 0)
-	backend := redisbackend.NewGR(cnf, []string{"localhost:6379"}, 0)
-	lock := eagerlock.New()
-	server := machinery.NewServer(cnf, broker, backend, lock)
+type GatherBlocksMsg {
+	Heights int[]   `json:"heights"`
+	ChainId string  `json:"chain_id"`
+}
 
-	tasks := map[string]interface{}{
-		"add":               exampletasks.Add,
-		"multiply":          exampletasks.Multiply,
-		"sum_ints":          exampletasks.SumInts,
-		"sum_floats":        exampletasks.SumFloats,
-		"concat":            exampletasks.Concat,
-		"split":             exampletasks.Split,
-		"panic_task":        exampletasks.PanicTask,
-		"long_running_task": exampletasks.LongRunningTask,
-	}
-
-	return server, server.RegisterTasks(tasks)
+type ChainTipMsg {
+	ChainTip  int  `json:"chain_tip"`
+	ChainId string  `json:"chain_id"`
 }
